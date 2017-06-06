63f03d74543382bfb2401f7a37aa3f238f46c2a7
YooJiSang <digndign2@gmail.com>
2017-06-07 02:00:19 +0900

    채팅 로그 생성부분 코드가 변경되었습니다.

--- cLogManager.cpp
+++ cLogManager.cpp
@@ -13,15 +13,9 @@ cLogManager::~cLogManager()
 
 void cLogManager::CreateLog(ST_CHAT stData)
 {
-	cout << stData.TEXT << endl;
 	string szFullPath = "Server/" + string(stData.ROOM_NAME) + ".txt";
 	ofstream writeFile(szFullPath.data(), ios::app);
-	if (writeFile.is_open()) {
-		writeFile << g_pTime->GetLocalTime() << " ";
-		writeFile << stData.TEXT;
-		writeFile.close();
-	}
-	else
+	if (!writeFile.is_open()) 
 	{
 		char strFolderPath[] = "Server";
 		int nResult = mkdir(strFolderPath);
@@ -32,12 +26,8 @@ void cLogManager::CreateLog(ST_CHAT stData)
 			perror("폴더 생성 실패 - 폴더가 이미 있거나 부정확함 \n");
 			cout << "errorno :" << errno << endl;
 		}
-		
-		ofstream writeFile(szFullPath, ios::app);
-		if (writeFile.is_open()) {
-			writeFile << g_pTime->GetLocalTime() << " ";
-			writeFile << stData.TEXT << "\n";
-			writeFile.close();
-		}
 	}
+	writeFile << g_pTime->GetLocalTime() << " ";
+	writeFile << stData.TEXT;
+	writeFile.close();
 }
